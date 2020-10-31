a7c439ca0bb900fe875dadce75cf4de61624e3f9
brentscott93 <bdscott@umass.edu>
2020-10-31 00:32:06 -0400

    update utils

--- R/utils_helpers.R
+++ R/utils_helpers.R
@@ -1,3 +1,49 @@
+
+#' Generates UI
+#' @description This is the 'muscle' behind the UI building and taken from the UI of shinyforms. It takes a list(id, type, etc.) and decides what to make. Used with lapply in main googleform() function over questions arguments.
+#' @param question A list containing id, type, required, and (optionally) choices
+formQ <- function(question){
+
+  #decide what widget to make
+  #all the shiny widgets have been renamed with wrappers to mimic the google form options
+  #built off of shinyforms
+  if (question$type == "numeric") {
+    input <- shiny::numericInput(question$id, NULL, 0)
+  } else if (question$type == "checkbox") {
+    input <- shiny::checkbox(question$id, question$choices)
+  } else if (question$type == 'multiplechoice'){
+    input <- multipleChoice(question$id, question$choices)
+  } else if (question$type == 'dropdown'){
+    input < dropdown(question$id, choices)
+  } else if (question$type == 'shortanswer'){
+    input <- shortAnswer(question$id)
+  } else if (question$type == 'paragraph'){
+    input <- paragraph(question$id)
+  } else if (question$type == 'height'){
+    input <- selectHeight(question$id)
+  }
+
+  #if questions is marked as required add a 'Required *' tag before widget
+  if(!is.null(question$required)){
+    if (question$required) {
+      ui <- shiny::tagList(h5('Required *', style = 'color:#fd0800;'), input)
+    } else {
+      ui <- input
+    }
+  } else {
+    ui <- input
+  }
+
+  #put everything ina dashboard box to make it look like a googleform
+  #one widget to a box
+  shinydashboard::box(width = NULL,
+                      solidHeader = T,
+                      title = question$title,
+                      ui )
+
+
+}
+
 #' Convenient wrappers for shiny widgets using the googleForm lingo
 #' @param id an inputId
 #' @param choices a list
